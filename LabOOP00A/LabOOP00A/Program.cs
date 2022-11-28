90743a1d63e9c621521d9658137a906963d3f5f7
Thidaratmatha05 <119285253+thirawat470012@users.noreply.github.com>
2022-11-28 15:47:20 +0700

    Update Data

---
+++ LabOOP00A/LabOOP00A/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Name : Thirawat Chottanakitpaisan");
+// id
+Console.WriteLine("Student ID :653450090-6");
+// email 
+Console.WriteLine("Email : thirawat.c@kkumail.com");
